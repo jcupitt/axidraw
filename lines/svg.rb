530439f6dac7335a22a1be1e7305611b184170b5
John Cupitt <jcupitt@gmail.com>
2018-05-13 14:28:03 +0100

    make a ruby version
    and an SVG generate class

---
+++ lines/svg.rb
@@ -0,0 +1,47 @@
+# simple SVG generator
+
+# example:
+#
+# Svg.new $stdout, viewBox: "0 0 1000 1000" do |svg|
+#     svg.line 0, 0, 400, 400, style: "stroke: black; stroke-width: 1" 
+# end
+
+class Svg
+    def attrs options
+        a = []
+        options.each do |name, value|
+            a << "#{name}=\"#{value}\""
+        end
+
+        a.join " "
+    end
+
+    def element name, options, block
+        if block
+            @output.puts "#{" " * @indent}<#{name} #{attrs(options)}>"
+            @indent += 2
+            block.call self
+            @indent -= 2
+            @output.puts "#{" " * @indent}</#{name}>"
+        else
+            @output.puts "#{" " * @indent}<#{name} #{attrs(options)}/>"
+        end
+    end
+
+    def initialize output, options={}, &block
+        @indent = 0
+        @output = output
+        options = options.merge xmlns: "http://www.w3.org/2000/svg"
+        element "svg", options, block
+    end
+
+    def g options={}, &block
+        element "g", options, block
+    end
+
+    def line x1, y1, x2, y2, options={}, &block
+        options = options.merge x1: x1, y1: y1, x2: x2, y2: y2
+        element "line", options, block
+    end
+
+end
