4e6363a607bc7e375354d44b25f1b5f001dcf5aa
John Cupitt <jcupitt@gmail.com>
2018-06-23 16:56:49 +0100

    house done, maybe

--- house/house.rb
+++ house/house.rb
@@ -8,37 +8,46 @@ $: << File.dirname(__FILE__)
 require 'svg'
 
 image = Vips::Image.new_from_file ARGV[0]
+edges = image.canny * 10
+lch = edges.colourspace "lch"
 
-# enough detail for what we need
-image = image.thumbnail 512
+$temp_number = 1
 
-temp_number = 1
 def primitive image
     image.write_to_file "x.png"
-    svg_name = "layer-#{temp_number}.svg"
+    svg_name = "layer-#{$temp_number}.svg"
+    $temp_number += 1
     puts "generating #{svg_name} ..."
-    if ! `primitive -i x.png -m 6 -n 1000 -o #{svg_name}`
+    if ! `primitive -i x.png -m 6 -n 100 -o #{svg_name}`
         exit
     end
 
     return svg_name
 end
 
-# to LCh, pull out reddish pixels 
+edge = (lch[0] > 0.05) & (lch[1] > 5) 
 
-kkkkkkk
+# pull out red / yellow pixels 25 - 80 hue
+red = edge & (lch[2] > 25) & (lch[2] < 80)
 
+# pull out greenish pixels 25 - 80 hue
+green = edge & (lch[2] > 80) & (lch[2] < 130)
 
+# pull out strong edges
+black = (lch[0] > 30) ^ (red | green)
 
+red_name = primitive red
+green_name = primitive green
+black_name = primitive black
 
-thresh = ARGV[0].to_i
-
+thresh = 50
 
 puts "writing x.svg ..."
 File.open "x.svg", "w" do |s|
-    Svg.new s, viewBox: "0 0 1000 1000" do |svg|
-        ARGV.drop(1).each do |filename|
-            svg.layer filename do |svg|
+    Svg.new s, viewBox: "0 0 1024 656" do |svg|
+        [red_name, green_name, black_name].each do |filename|
+            svg.layer filename, 
+                transform: "scale(4.000000) translate(0.5 0.5)" do |svg|
                 File.foreach(filename).with_index do |line, line_num|
                     if line =~ /stroke="#([0-9a-f]+)"/
                         hex = $~[1]
---
+++ lines2/project.rb
@@ -0,0 +1,118 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'vips'
+
+# get the files from the same directory as us
+$: << File.dirname(__FILE__) 
+
+require 'svg'
+
+$options = {
+	:verbose => true,
+	:fatal => false,
+	:output => "x.svg",
+}
+OptionParser.new do |opts|
+	opts.banner = "Usage: #{$0} [options] input-image"
+
+	opts.on("-o", "--output=FILE", "output to FILE") do |file|
+		$options[:output] = file
+	end
+	opts.on("-f", "--fatal", "Stop on first error") do |v|
+		$options[:fatal] = v
+	end
+	opts.on("-q", "--quiet", "Run quietly") do |v|
+		$options[:verbose] = false
+	end
+end.parse!
+
+def log(msg)
+	if $options[:verbose]
+		puts msg
+	end
+end
+
+def err(msg)
+	puts msg
+
+	if $options[:fatal]
+		exit
+	end
+end
+
+# number of segments ...  12 is a clock face, for example
+ANGLES = 120
+
+class Lines
+    def initialize(image)
+    end
+
+    def simulate(length, vectors)
+        bg = Vips::Image.black length, length 
+        vectors.each_with_index do |image, i|
+            strips = image.zoom(image.height, 1).rotate(i * 360 / ANGLES)
+            bg += strips.gravity('centre', length, length)
+        end
+
+        bg
+    end
+
+end
+
+log "loading #{ARGV[0]} ..." 
+image = Vips::Image.new_from_file ARGV[0]
+image = image.invert()
+
+if image.width != image.height
+    err "image must be square"
+end
+diameter = image.width 
+radius = diameter / 2
+
+# mask to a circle
+log "generating vectors ..."
+mask = Vips::Image.black image.width, image.height
+mask = mask.draw_circle 255, radius, radius, radius, fill: true
+image = mask.ifthenelse image, 0 
+
+vectors = []
+(0 ... ANGLES / 2).each do |i|
+    angle = -i * 360 / ANGLES
+    columns, rows = image.rotate(angle).project()
+    vectors << rows
+end
+
+length = 0
+max_value = 0
+vectors.each do |image|
+    length = [length, image.height].max
+    max_value = [max_value, image.max()].max
+end
+
+# log "writing simulated output ..."
+# bg.write_to_file "x.v"
+
+# make the image vectors into simple Ruby arrays
+arrays = vectors.map {|vector| vector.to_a.transpose[0].transpose[0]}
+
+log "writing SVG to #{$options[:output]} ..."
+File.open $options[:output], "w" do |s|
+    Svg.new s, viewBox: "0 0 1000 1000" do |svg|
+        svg.g transform: "translate(500, 500)", 
+            style: "stroke: black; stroke-width: 1" do |svg|
+            arrays.each_with_index do |array, i|
+                svg.g transform: "rotate(#{i * -360 / ANGLES})" do |svg|
+                    (0 ... array.length).each do |j|
+                        prob = array[j] / max_value
+                        ln = j - array.length / 2
+                        y = ln * 400 / (array.length / 2)
+
+                        svg.line(-400, y, 400, y) if 4 * rand < prob
+                    end
+                end
+            end
+        end
+    end
+end
+
